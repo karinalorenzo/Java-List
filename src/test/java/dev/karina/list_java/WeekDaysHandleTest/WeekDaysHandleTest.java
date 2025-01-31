a7abcee86f7ea46839c2e7e028c25990fd5fc76e
karinareyes25 <karinalrg16@gmail.com>
2025-01-31 15:11:46 +0100

    test for WeekDaysHandle empty

--- src/test/java/dev/karina/list_java/WeekDaysHandleTest/WeekDaysHandleTest.java
+++ src/test/java/dev/karina/list_java/WeekDaysHandleTest/WeekDaysHandleTest.java
@@ -1,5 +1,55 @@
 package dev.karina.list_java.WeekDaysHandleTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
 public class WeekDaysHandleTest {
+    private WeekDaysHandle manager;
+
+@BeforeEach
+void setUp(){
+    manager = new WeekDaysHandle();
+}
+
+@Test
+void testCreateList(){
+
+}
+
+@Test
+void testGetDaysWeek(){
     
+}
+@Test
+void testGetSizeList(){
+    
+}
+
+@Test
+void testDeleteDays(){
+    
+}
+
+@Test
+void testGetSolicitedDay(){
+    
+}
+
+@Test
+void testExistDay(){
+    
+}
+
+@Test
+void testOrderDays(){
+
+}
+
+@Test
+void testEmptyList(){
+
+}
+
 }
